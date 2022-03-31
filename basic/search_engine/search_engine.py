1fb361118fafec47363a2968a4095724ae54acd9
sunjingjun <sjj990221@163.com>
2022-03-31 18:58:39 +0800

    搜索引擎学习

---
+++ basic/search_engine/search_engine.py
@@ -0,0 +1,166 @@
+import re
+
+
+class SearchEngineBase(object):
+    def __init__(self):
+        pass
+
+    # 负责读取文件内容，将文件路径作为 ID，连同内容一起送到 process_corpus 中。
+    def add_corpus(self, file_path):
+        with open(file_path, 'r') as fin:
+            text = fin.read()
+        self.process_corpus(file_path, text)
+
+    # 需要对内容进行处理，然后文件路径为 ID ，将处理后的内容存下来。处理后的内容，就叫做索引（index）
+    def process_corpus(self, id, text):
+        raise Exception('process_corpus not implemented.')
+
+    # 给定一个询问，处理询问，再通过索引检索，然后返回
+    def search(self, query):
+        raise Exception('search not implemented.')
+
+
+def main(search_engine):
+    for file_path in ['1.txt', '2.txt', '3.txt', '4.txt', '5.txt']:
+        search_engine.add_corpus(file_path)
+
+    while True:
+        query = input()
+        results = search_engine.search(query)
+        print('found {} result(s):'.format(len(results)))
+        for result in results:
+            print(result)
+
+
+class SimpleEngine(SearchEngineBase):
+    def __init__(self):
+        super(SimpleEngine, self).__init__()
+        self.__id_to_texts = {}
+
+    def process_corpus(self, id, text):
+        self.__id_to_texts[id] = text
+
+    def search(self, query):
+        results = []
+        for id, text in self.__id_to_texts.items():
+            if query in text:
+                # 这里是当时看代码疑问最大的地方，text是一个str类型，可以直接使用in进行字符串的匹配
+                print(type(text))
+                results.append(id)
+        return results
+
+
+# search_engine = SimpleEngine()
+# main(search_engine)
+
+# 不再是存储整篇文章为str，而是将文章打碎成set，来进行单词的检索，而且增加了多个单词定位一篇文章的功能
+# 但是多个单词的顺序和连贯性等都没有保证
+class BOWEngine(SearchEngineBase):
+    def __init__(self):
+        super(BOWEngine, self).__init__()
+        self.__id_to_words = {}
+
+    def process_corpus(self, id, text):
+        self.__id_to_words[id] = self.parse_text_to_words(text)
+
+    def search(self, query):
+        query_words = self.parse_text_to_words(query)
+        results = []
+        for id, words in self.__id_to_words.items():
+            if self.query_match(query_words, words):
+                results.append(id)
+        return results
+
+    @staticmethod
+    def query_match(query_words, words):
+        for query_word in query_words:
+            if query_word not in words:
+                return False
+        return True
+
+    @staticmethod
+    def parse_text_to_words(text):
+        # 使用正则表达式去除标点符号和换行符
+        text = re.sub(r'[^\w ]', ' ', text)
+        # 转为小写
+        text = text.lower()
+        # 生成所有单词的列表
+        word_list = text.split(' ')
+        # 去除空白单词
+        word_list = filter(None, word_list)
+        # 返回单词的 set
+        return set(word_list)
+
+
+# search_engine = BOWEngine()
+# main(search_engine)
+
+#
+class BOWInvertedIndexEngine(SearchEngineBase):
+    def __init__(self):
+        super(BOWInvertedIndexEngine, self).__init__()
+        # 存储到排序索引，比如my在123篇文章中都出现了  my:1,2,3
+        self.inverted_index = {}
+
+    def process_corpus(self, id, text):
+        words = self.parse_text_to_words(text)
+        for word in words:
+            if word not in self.inverted_index:
+                self.inverted_index[word] = []
+            self.inverted_index[word].append(id)
+
+    def search(self, query):
+        query_words = list(self.parse_text_to_words(query))
+        query_words_index = list() # [0,0,0,0,0] 有几个查询单词，就有几个0，相当于初始化
+        for query_word in query_words:
+            query_words_index.append(0)
+
+        # 如果某一个查询单词的倒序索引为空，我们就立刻返回
+        for query_word in query_words:
+            if query_word not in self.inverted_index:
+                return []
+
+        result = []
+        while True:
+
+            # 首先，获得当前状态下所有倒序索引的 index
+            current_ids = []
+
+            for idx, query_word in enumerate(query_words):
+                # query_words_index ???
+                current_index = query_words_index[idx]
+                current_inverted_list = self.inverted_index[query_word]
+
+                # 已经遍历到了某一个倒序索引的末尾，结束 search
+                if current_index >= len(current_inverted_list):
+                    return result
+
+                current_ids.append(current_inverted_list[current_index])
+
+            # 然后，如果 current_ids 的所有元素都一样，那么表明这个单词在这个元素对应的文档中都出现了
+            if all(x == current_ids[0] for x in current_ids):
+                result.append(current_ids[0])
+                query_words_index = [x + 1 for x in query_words_index]
+                continue
+
+            # 如果不是，我们就把最小的元素加一
+            min_val = min(current_ids)
+            min_val_pos = current_ids.index(min_val)
+            query_words_index[min_val_pos] += 1
+
+    @staticmethod
+    def parse_text_to_words(text):
+        # 使用正则表达式去除标点符号和换行符
+        text = re.sub(r'[^\w ]', ' ', text)
+        # 转为小写
+        text = text.lower()
+        # 生成所有单词的列表
+        word_list = text.split(' ')
+        # 去除空白单词
+        word_list = filter(None, word_list)
+        # 返回单词的 set
+        return set(word_list)
+
+
+search_engine = BOWInvertedIndexEngine()
+main(search_engine)
